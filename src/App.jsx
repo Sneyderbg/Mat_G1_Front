1d77e66b98f52df58c773df2eb01c97265d84c56
Sneyderbg <sneyder.buitrago@udea.edu.co>
2023-05-18 16:09:20 -0500

    refactor: use environment variables instead of a config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "App.css";
 import { MainPage } from "components/MainPage";
-import cfg from "utils/config.json";
 
 function App() {
   return (
     <div>
-      <MainPage cfg={cfg} />
+      <MainPage />
     </div>
   );
 }
