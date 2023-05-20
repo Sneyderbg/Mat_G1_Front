52f50b89cae61e2463ef82bc6104aa3e9961ce22
Sneyderbg <sneyder.buitrago@udea.edu.co>
2023-05-19 20:21:30 -0500

    feat: implement the sending of the registration to the back-end server

--- src/utils/CommonRequests.js
+++ src/utils/CommonRequests.js
@@ -13,18 +13,21 @@ export const STATUS = {
  * @param {Number} userId Id de usuario.
  * @returns Promise de la info.
  */
-export function getUserInfo(userId) {
-  return axios
-    .get(`${process.env.REACT_APP_STUDENTS_ENDPOINT}/${userId}`, {
-      signal:
-        process.env.REACT_APP_BYPASS_TIMEOUTS === "1"
-          ? undefined
-          : AbortSignal.timeout(5000),
-    })
-    .then((res) => ({ ...res.data, status: STATUS.OK }))
-    .catch((err) =>
-      handleError(err, "Error al obtener la información del usuario.")
+export async function getUserInfo(userId) {
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_STUDENTS_ENDPOINT}/${userId}`,
+      {
+        signal:
+          process.env.REACT_APP_BYPASS_TIMEOUTS === "1"
+            ? undefined
+            : AbortSignal.timeout(5000),
+      }
     );
+    return { ...res.data, status: STATUS.OK };
+  } catch (err) {
+    return handleError(err, "Error al obtener la información del usuario.");
+  }
 }
 
 export function getOferta(idOferta) {
@@ -62,6 +65,26 @@ export function getGroupsByCourseId(courseId) {
     .catch((err) =>
       handleError(err, "Error al cargar los grupos para esta materia.")
     );
+}
+
+export function sendMatricula(infoMatricula) {
+  const body = {
+    estudianteId: infoMatricula.idEstudiante,
+    semestre: infoMatricula.semestre,
+    gruposId: infoMatricula.selectedGroups
+      .filter((group) => group.groupSelected)
+      .map((group) => group.grupoId),
+  };
+  return axios
+    .post(
+      process.env.REACT_APP_REGISTRATION_ENDPOINT,
+      { ...body },
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+    .then((res) => res)
+    .catch((err) => handleError(err, "Error al enviar la matricula."));
 }
 
 function handleError(err, customMsg) {
